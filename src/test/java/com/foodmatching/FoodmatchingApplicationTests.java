f9f2b8be1544de260565342f7ad4cacb666d9a89
Shinhong Park <shin@Shinhongui-MacBook-Pro.local>
2016-11-25 07:40:46 +0900

    set static

--- src/test/java/com/foodmatching/FoodmatchingApplicationTests.java
+++ src/test/java/com/foodmatching/FoodmatchingApplicationTests.java
@@ -1,16 +1,34 @@
 package com.foodmatching;
 
+import javax.annotation.Resource;
+
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.foodmatching.dao.UserDao;
+import com.foodmatching.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ActiveProfiles(profiles = "test")
 public class FoodmatchingApplicationTests {
+	@Autowired
+    @Qualifier("anotherDAO")
+    private UserDao userDao;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Resource(name = "anotherService")
+    private UserService userService;
+
+
+    @Test
+    public void aTest() {
+        
+    }
 
 }
