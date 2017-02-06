a3664155312543fc5762c1201a2593e35731d221
Shinhong Park <psh01111@gmail.com>
2017-02-06 22:37:58 +0900

    Security 처리

--- src/main/java/com/foodmatching/config/SecurityConfig.java
+++ src/main/java/com/foodmatching/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.foodmatching.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,35 +15,39 @@ import com.foodmatching.serviceimpl.UserServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserServiceImpl userService;
-	
+
+	//@Override
+	//public void configure(WebSecurity web) throws Exception {
+		//web.ignoring().antMatchers("//resources/static/**");
+	//}
+
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-		 http.csrf().disable()
-         .authorizeRequests()
-             .antMatchers("/resources/**","/").permitAll() 
-             .anyRequest().authenticated()
-             .and()
-         .formLogin()
-             .loginPage("/login")
-             .permitAll()
-             .and()
-             
-         .logout()                                    
-             .permitAll();
-       
-    }
- 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
- 
-	
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/images/**","/css/**","/bootstrap/**", "/").permitAll()
+		.anyRequest().authenticated();
+
+		http
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.and()
+			.logout().permitAll();
+
+		http.csrf().disable();
+
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 
 	}
