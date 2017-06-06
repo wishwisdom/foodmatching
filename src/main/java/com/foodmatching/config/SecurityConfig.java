17d74542ef632a43d3a0e37f4509e0c1b1eaed21
Shinhong Park <psh01111@gmail.com>
2017-06-06 15:45:05 +0900

    fix an error which is can not read js file in /js path

--- src/main/java/com/foodmatching/config/SecurityConfig.java
+++ src/main/java/com/foodmatching/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/images/**","/img/**","/css/**","/bootstrap/**","/register", "/*").permitAll()
+		.antMatchers("/images/**","/img/**","/css/**","/bootstrap/**","/js/**","/register", "/*").permitAll()
 		.anyRequest().authenticated();
 
 		http
